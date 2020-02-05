930d28db756a7c70f478463269935ba603f34e74
Daniel Gafni <danielgafni16@gmail.com>
2020-02-06 02:40:58 +0300

    added resnet18

--- models.py
+++ models.py
@@ -22,18 +22,83 @@ class BaselineNN(nn.Module):
 
 
 class LSTM(nn.Module):
-
-    def __init__(self, hidden_dim, seq_len=2):
+    def __init__(self, hidden_dim, input_dim=13, seq_len=5):
         super(LSTM, self).__init__()
-        self.input_dim = 7
+        self.input_dim = input_dim
         self.hidden_dim = hidden_dim
-        self.lstm = torch.nn.LSTM(7, self.hidden_dim, batch_first=True)
-        self.linear = nn.Linear(seq_len * self.hidden_dim, 6)
+        self.lstm = torch.nn.LSTM(self.input_dim, self.hidden_dim, batch_first=True)
+        self.linear1 = torch.nn.Linear(seq_len * hidden_dim, 6)
         self.tanh = nn.Tanh()
+        self.linear2 = nn.Linear(6, 6)
+        self.linear2.weight.data.fill_(15000)
 
     def forward(self, input_seq):
         batch_size = input_seq.size(0)
         lstm_out, _ = self.lstm(input_seq)
-        predictions = self.linear(lstm_out.reshape(batch_size, -1))
-        predictions = self.tanh(predictions)
+        # predictions = self.linear1()
+        # predictions = self.tanh(predictions)
+        predictions = self.linear1(lstm_out.reshape(batch_size, -1))
+
         return predictions
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv1d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm1d(planes)
+        self.conv2 = nn.Conv1d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm1d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv1d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm1d(self.expansion*planes)
+                )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks):
+        super(ResNet, self).__init__()
+        self.in_planes = 13
+
+        self.conv1 = nn.Conv1d(13, 13, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm1d(13)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.linear = nn.Linear(256, 6)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 2)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
+
+def ResNet18():
+    return ResNet(BasicBlock, [2,2,2,2])
--- utils.py
+++ utils.py
@@ -5,32 +5,18 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 import plotly.graph_objs as go
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+
+features = ['epoch',
+            'x_sim', 'y_sim', 'z_sim', 'Vx_sim', 'Vy_sim', 'Vz_sim',
+            'ro_sim', 'theta_sim', 'fi_sim', 'dro/dt_sim', 'dtheta/dt_sim', 'dfi/dt_sim']
+targets = ['x', 'y', 'z', 'Vx', 'Vy', 'Vz']
 
 
 def smape(satellite_predicted_values, satellite_true_values):
     # Score function
     return torch.mean(torch.abs((satellite_predicted_values - satellite_true_values)
                           / (torch.abs(satellite_predicted_values) + torch.abs(satellite_true_values))))
-
-
-def spherical_from_cartesian(data):
-    data['ro'] = np.sqrt(data['x'] ** 2 + data['y'] ** 2 + data['z'] ** 2)
-    data['ro_sim'] = np.sqrt(data['x_sim'] ** 2 + data['y_sim'] ** 2 + data['z_sim'] ** 2)
-    data['theta'] = np.arccos(data['x'] / data['ro'])
-    data['theta_sim'] = np.arccos(data['x_sim'] / data['ro_sim'])
-    data['fi'] = np.arctan(data['z'] / data['y'])
-    data['fi_sim'] = np.arctan(data['z_sim'] / data['y_sim'])
-    data['dro/dt'] = (data['Vx'] * data['x'] + data['Vy'] * data['y'] + data['Vz'] * data['z']) / data['ro']
-    data['dro/dt_sim'] = (data['Vx_sim'] * data['x_sim'] + data['Vy_sim'] * data['y_sim'] + data['Vz_sim'] * data['z_sim']) / data['ro_sim']
-    data['dtheta/dt'] = (data['Vx'] * data['y'] - data['Vy'] * data['x']) / np.sqrt(data['x'] ** 2 + data['y'] ** 2)
-    data['dtheta/dt_sim'] = (data['Vx_sim'] * data['y_sim'] - data['Vy_sim'] * data['x_sim']) / np.sqrt(data['x_sim'] ** 2 + data['y_sim'] ** 2)
-    data['dfi/dt'] = ((data['z'] * (data['x'] * data['Vx'] + data['y'] * data['Vy']) - (data['x'] ** 2 + data['y'] ** 2) * data['Vz'])
-                      /(data['ro'] ** 2 * np.sqrt(data['x'] ** 2 + data['y'] ** 2)))
-    data['dfi/dt_sim'] = ((data['z_sim'] * (data['x_sim'] * data['Vx_sim'] + data['y_sim'] * data['Vy_sim']) - (data['x_sim'] ** 2 + data['y_sim'] ** 2) * data['Vz_sim'])
-                          /(data['ro_sim'] ** 2 * np.sqrt(data['x_sim'] ** 2 + data['y_sim'] ** 2)))
-    data = data.drop(['x', 'y', 'z', 'Vx', 'Vy', 'Vz', 'x_sim', 'y_sim', 'z_sim', 'Vx_sim', 'Vy_sim', 'Vz_sim'], axis=1)
-    return data
 
 
 class CustomDataset(Dataset):
@@ -47,21 +33,37 @@ class CustomDataset(Dataset):
         return self.x[item:item + self.seq_len], self.y[item + self.seq_len - 1]
 
 
+def spherical_from_cartesian(data):
+    data['ro_sim'] = np.sqrt(data['x_sim'] ** 2 + data['y_sim'] ** 2 + data['z_sim'] ** 2)
+    data['theta_sim'] = np.arccos(data['x_sim'] / data['ro_sim'])
+    data['fi_sim'] = np.arctan(data['z_sim'] / data['y_sim'])
+    data['dro/dt_sim'] = (data['Vx_sim'] * data['x_sim'] + data['Vy_sim'] * data['y_sim'] + data['Vz_sim'] * data['z_sim']) / data['ro_sim']
+    data['dtheta/dt_sim'] = (data['Vx_sim'] * data['y_sim'] - data['Vy_sim'] * data['x_sim']) / np.sqrt(data['x_sim'] ** 2 + data['y_sim'] ** 2)
+    data['dfi/dt_sim'] = ((data['z_sim'] * (data['x_sim'] * data['Vx_sim'] + data['y_sim'] * data['Vy_sim']) - (data['x_sim'] ** 2 + data['y_sim'] ** 2) * data['Vz_sim'])
+                          /(data['ro_sim'] ** 2 * np.sqrt(data['x_sim'] ** 2 + data['y_sim'] ** 2)))
+    return data
+
+
 def process(data):
-    features = ['epoch', 'ro_sim', 'theta_sim', 'fi_sim', 'dro/dt_sim', 'dtheta/dt_sim', 'dfi/dt_sim']
-    targets = ['ro', 'theta', 'fi', 'dro/dt', 'dtheta/dt', 'dfi/dt']
+    data = data.drop(['id'], axis=1)
+    # Convert coordinates
+    # data = spherical_from_cartesian(data)
     # Convert date and time to seconds
     data['epoch'] = data['epoch'].apply(lambda x: x.to_pydatetime().timestamp())
-    data['epoch'] = data['epoch'] - data['epoch'].min()
-    # Convert coorinates to spherical
+    data['epoch'] = (data['epoch'] - data['epoch'].min())
+    # generate spherical coordinates features
     data = spherical_from_cartesian(data)
-    data[features] = data[features] / np.abs(data[features]).max(axis=0)
-    data[targets] = data[targets] / np.abs(data[targets]).max(axis=0)
+    # Scale features
+    data_to_scale = data.drop(['sat_id', 'x', 'y', 'z', 'Vx', 'Vy', 'Vz'], axis=1)
+    scaler = StandardScaler()
+    scaler.fit(data_to_scale)
+    data_scaled = scaler.transform(data_to_scale)
+    data[features] = data_scaled
     # Split by satellite id
     sat_datas = []
     data_grouped = data.groupby('sat_id')
     for sat_data in data_grouped:
-        sat_datas.append(sat_data[1].drop(['id', 'sat_id'], axis=1))
+        sat_datas.append(sat_data[1].drop(['sat_id'], axis=1))
     sat_datas_train = []
     sat_datas_test = []
     for sat_data in sat_datas:
@@ -69,4 +71,4 @@ def process(data):
         sat_data_train, sat_data_test = train_test_split(sat_data, shuffle=False)
         sat_datas_train.append(sat_data_train)
         sat_datas_test.append(sat_data_test)
-    return sat_data_train, sat_data_test
+    return sat_datas_train, sat_datas_test
